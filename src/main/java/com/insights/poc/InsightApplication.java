d9533c66a3f6f3e6ab715f3f63750704dac86c51
Atul Bhombe <atul.bhombe@gmail.com>
2024-09-23 10:18:29 +0530

    Moved repository and entity to separate package

--- src/main/java/com/insights/poc/InsightApplication.java
+++ src/main/java/com/insights/poc/InsightApplication.java
@@ -1,5 +1,7 @@
 package com.insights.poc;
 
+import com.insights.poc.entity.Customer;
+import com.insights.poc.repository.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
--- src/test/java/com/insights/poc/CustomerRepositoryTests.java
+++ src/test/java/com/insights/poc/CustomerRepositoryTests.java
@@ -1,5 +1,7 @@
 package com.insights.poc;
 
+import com.insights.poc.entity.Customer;
+import com.insights.poc.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
